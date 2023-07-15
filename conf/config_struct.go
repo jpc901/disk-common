845005cad2364c7659f5a7517f33853bfe82f280
jiapengcheng <pengchengjia@deepglint.com>
2023-07-15 18:39:27 +0800

    feat: 添加db配置

--- conf/config_struct.go
+++ conf/config_struct.go
@@ -5,12 +5,23 @@ type Config struct {
 	Version       string `mapstructure:"version"`
 	*ServerConfig `mapstructure:"server"`
 	*LogConfig    `mapstructure:"log"`
+	*MySQLConfig  `mapstructure:"mysql"`
 }
 
 type ServerConfig struct {
-	Port string `mapstructure:"port"`
+	Port int `mapstructure:"port"`
 }
 
 type LogConfig struct {
 	Level string `mapstructure:"level"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DB           string `mapstructure:"dbname"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
 }
