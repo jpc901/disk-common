c15d20355a198ef48a39a8c8bb1f6e72b419f4d8
jiapengcheng <pengchengjia@deepglint.com>
2023-07-17 00:23:30 +0800

    feat: add jwt snowflake

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -3,16 +3,16 @@ package jwt
 import (
 	"fmt"
 
-	"github.com/jpc901/disk-common/util"
+	"github.com/jpc901/disk-common/utils"
 )
 
-// CreateToken 根据uuid和有效期截止时间生成token
+// CreateToken 根据uid和有效期截止时间生成token
 func CreateToken(uuid, timeStamp int64) (string, error) {
-	return util.RsaEncrypt(fmt.Sprintf("%d:%d", uuid, timeStamp))
+	return utils.RsaEncrypt(fmt.Sprintf("%d:%d", uuid, timeStamp))
 }
 
 func ParseToken(token string) (uuid, timeStamp int64, err error) {
-	parseToken, err := util.RsaDecrypt(token)
+	parseToken, err := utils.RsaDecrypt(token)
 	if err != nil {
 		return
 	}
